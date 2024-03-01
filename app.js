585288c260e9092c73a4584cb79a0b17073564d2
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-03-01 13:31:45 +0530

    update

---
+++ app.js
@@ -0,0 +1,202 @@
+// Question no-01:
+// 1. Declare a variable called username.
+
+// Solution:
+
+// var username =
+
+// Question no-02:
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name..
+
+// Solution:
+
+// var myName = "Muhammad Arsalan"
+
+// Question no-03:
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+
+// Solution:
+
+// var message = "Hello World !";
+// alert(message);
+
+
+// Question no-04:
+// 4.Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+// Solution:
+
+// var studentName = 'Muhammad Arsalan'
+// var age = 18;
+// var education = ('Certified Mobile App Development'); 
+
+//This is in  Alert Function:
+
+// alert("Name: " + studentName);
+// alert("Age : "+ age);
+// alert("Education : "+ education);
+
+// !!OR!!
+
+//This is in  Promt  Function:
+
+// var studentName = prompt("Enter student name!");
+// var age = prompt("Enter Age!");
+// var education = prompt("Enter your education!");
+
+// console.log("Studentname : " + studentName);
+// console.log("Age : " + age);
+// console.log("Education : " + education);
+
+// !!OR!!
+
+//This is in  Browser's  Function:
+
+// document.write("Studentname : " + studentName);
+// document.write("Fathername : " + fatherName);
+// document.write("Age : " + age);
+// document.write("Roll no : " + rollNo);
+
+
+// Question no-05:
+//5. Write a script to display the following alert using one JS 
+// variable.
+
+// Solution:
+
+// var pizza = "PIZZA"
+// var pizz = "PIZZ"
+// var piz = "PIZ"
+// var pi = "PI"
+// var p = "P"
+// alert(pizza + pizz + piz + pi +p);
+
+alert("PIZZA \n" +"PIZZ \n" + "PIZ \n" + "PI \n" + "P \n");
+
+
+// Question no-06:
+
+//6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation...
+
+// Solution:
+
+// var email = "muhammadarsalanforever@gmail.com"
+
+// alert("My E-mail Address is " + email);
+
+
+// Question no-07:
+
+//7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+// Solution:
+
+// var book = " A smarter way to learn JavaScript.."
+
+// alert("I am trying to learn javascript from the Book "+ book);
+
+
+// Question no-08:
+
+// 8. Write a script to display this in browser through JS
+
+// Solution:
+
+// document.write("<h2>"+ "Yeh! I can write HTML content through Javascript" +"</h2>")
+
+
+// Question no-09:
+
+// 9. Store following string in a variable and show in alert and 
+// browser through JS...
+
+// Solution:
+
+
+// var style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(style);
+
+// document.write(style);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
